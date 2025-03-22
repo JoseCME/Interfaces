3c11578ff191bef8e92564a80c0a40125ae716c5
Jose <146311174+JoseCME@users.noreply.github.com>
2025-03-22 10:56:56 -0600

    Add files via upload
    Subi las clases pedidas

---
+++ Moto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1bpoo.MisClases
+{
+    internal class Moto : Vehiculo
+    {
+        public Moto(int anio, string elColor, string elModelo) : base(anio, elColor, elModelo)
+        {
+            TiposLicenciaAceptado = new List<string> {"M"};
+        }
+
+    }
+}
---
+++ Trailer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1bpoo.MisClases
+{
+    internal class Trailer : Vehiculo
+    {
+        private string numerodeejes;
+        private string gasolina;
+        public Trailer(int anio, string elColor, string elModelo, string numerodeejes, string gasolina) : base(anio, elColor, elModelo)
+        {
+            TiposLicenciaAceptado = new List<string> { "A" };
+            this.numerodeejes = numerodeejes;
+            this.gasolina = gasolina;
+        }
+
+        public override void acelerar(int cuanto)
+        {
+            base.acelerar(cuanto);
+            
+        }
+    }
+}
